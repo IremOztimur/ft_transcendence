35ce9145f0db3fcf773b498f6d746d565c0147ac
IremOztimur <iremoztimur07@gmail.com>
2024-08-19 23:33:29 +0300

    [CREATED] game manager class

---
+++ game.js
@@ -0,0 +1,23 @@
+class Game {
+	constructor(context, width, height) {
+		this.context = context;
+		this.width = width;
+		this.height = height;
+		this.ball = new Ball(vec2(200, 200), vec2(2, 2), 20);
+	}
+
+	update() {
+		this.ball.update();
+	}
+
+	draw() {
+		this.context.clearRect(0, 0, this.width, this.height);
+		this.ball.draw(this.context);
+	}
+
+	loop() {
+		this.update();
+		this.draw();
+		requestAnimationFrame(() => this.loop());
+	}
+}
