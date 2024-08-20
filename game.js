18a297e652284553e9de6b090e55256b225ad7a7
IremOztimur <iremoztimur07@gmail.com>
2024-08-20 12:31:44 +0300

    [ADDED] Handled ball collision with the edges

--- game.js
+++ game.js
@@ -3,21 +3,32 @@ class Game {
 		this.context = context;
 		this.width = width;
 		this.height = height;
-		this.ball = new Ball(vec2(200, 200), vec2(2, 2), 20);
+		this.ball = new Ball(vec2(200, 200), vec2(5, 5), 20);
+	}
+
+	ballCollisionWithTheEdges() {
+		if (this.ball.pos.y + this.ball.radius > this.height || this.ball.pos.y - this.ball.radius <= 0) {
+			this.ball.velocity.y *= -1;
+		}
+
+		if (this.ball.pos.x + this.ball.radius > this.width || this.ball.pos.x - this.ball.radius <= 0) {
+			this.ball.velocity.x *= -1;
+		}
 	}
 
 	update() {
 		this.ball.update();
+		this.ballCollisionWithTheEdges();
 	}
 
-	draw() {
+	render() {
 		this.context.clearRect(0, 0, this.width, this.height);
 		this.ball.draw(this.context);
 	}
 
 	loop() {
 		this.update();
-		this.draw();
+		this.render();
 		requestAnimationFrame(() => this.loop());
 	}
 }
