2ba70d79903ed1debdf2d89d65238beb4beb65ea
IremOztimur <iremoztimur07@gmail.com>
2024-08-22 15:32:07 +0300

    [ADDED] background-image

--- src/game/game.js
+++ src/game/game.js
@@ -6,8 +6,19 @@ class Game {
 		this.ball = new Ball(vec2(200, 200), vec2(12, 12), 20);
 		this.paddle1 = new Paddle(vec2(0, 50), vec2(10, 10), 20, 160);
 		this.paddle2 = new Paddle(vec2(width-20, 200), vec2(10, 10), 20, 160);
-	}
 
+		this.backgroundImage = new Image();
+		this.backgroundImage.src = "/Users/iremoztimur/Projects/ping-pong/src/assets/neon-grid.jpg";
+		this.backgroundLoaded = false;
+
+		this.backgroundImage.onload = () => {
+			this.backgroundLoaded = true;
+		};
+
+		this.backgroundImage.onerror = () => {
+			console.error("Failed to load the background image.");
+		};
+	}
 
 	update(keysPressed) {
 		this.ball.update();
@@ -20,12 +31,16 @@ class Game {
 	}
 
 	render(isPaused) {
-		this.context.fillStyle = "rgba(0, 0, 0, 0.4)";
-		this.context.fillRect(0, 0, this.width, this.height);
+		if (this.backgroundLoaded) {
+			this.context.drawImage(this.backgroundImage, 0, 0, this.width, this.height);
+		} else {
+			this.context.fillStyle = "rgba(0, 0, 0, 0.4)";
+			this.context.fillRect(0, 0, this.width, this.height);
+		}
 		this.ball.draw(this.context);
 		this.paddle1.draw(this.context);
 		this.paddle2.draw(this.context);
-		drawGameFrame(this);
+		//drawGameFrame(this);
 
 		if (isPaused)
 			pauseTable(this);
