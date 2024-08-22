e1a1952e8aba9559919cde30423b401daa3fcc68
IremOztimur <iremoztimur07@gmail.com>
2024-08-22 17:16:17 +0300

    [CREATED] multiplayer option added

--- src/game/game.js
+++ src/game/game.js
@@ -1,8 +1,9 @@
 class Game {
-	constructor(context, width, height) {
+	constructor(context, width, height, mode) {
 		this.context = context;
 		this.width = width;
 		this.height = height;
+		this.mode = mode;
 		this.ball = new Ball(vec2(200, 200), vec2(12, 12), 20);
 		this.paddle1 = new Paddle(vec2(0, 50), vec2(10, 10), 20, 160);
 		this.paddle2 = new Paddle(vec2(width-20, 200), vec2(10, 10), 20, 160);
@@ -12,10 +13,14 @@ class Game {
 	update(keysPressed) {
 		this.ball.update();
 		this.paddle1.update(keysPressed);
+		if (this.mode === "ai") {
+			AIPlayer(this);
+		} else if (this.mode === "multiplayer") {
+			this.paddle2.update(keysPressed);
+		}
 		ballCollisionWithTheEdges(this);
 		paddleCollisionWithTheEdges(this);
 		ballCollisionWithThePaddle(this);
-		AIPlayer(this);
 		gameScore(this);
 	}
 
@@ -32,9 +37,9 @@ class Game {
 	}
 
 	loop(keysPressed, isPaused) {
-		if (!isPaused)
+		if (!isPaused) {
 			this.update(keysPressed);
-
+		}
 		this.render(isPaused);
 	}
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 window.onload = function() {
 	const canvas = document.getElementById("pong");
 	const context = canvas.getContext('2d');
+	const menu = document.getElementById("menu");
 
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+	canvas.style.display = 'none';
 
 	const keysPressed = [];
 	let isPaused = false;
+
+	document.getElementById("aiMode").addEventListener("click", function() {
+		initGame("ai");
+	});
+
+	document.getElementById("multiplayerMode").addEventListener("click", function() {
+		initGame("multiplayer");
+	});
 
 	window.addEventListener('keydown', function(event)
 	{
@@ -23,12 +33,17 @@ window.onload = function() {
 		keysPressed[event.keyCode] = false;
 	});
 
-	const game = new Game(context, canvas.width, canvas.height);
 
-	function gameLoop() {
-		game.loop(keysPressed, isPaused);
-		requestAnimationFrame(gameLoop);
+	function initGame(mode) {
+		menu.style.display = 'none';
+		canvas.style.display = 'block';
+		const game = new Game(context, canvas.width, canvas.height, mode);
+
+		function gameLoop() {
+			game.loop(keysPressed, isPaused);
+			requestAnimationFrame(gameLoop);
+		}
+
+		gameLoop();
 	}
-
-	gameLoop();
 };
